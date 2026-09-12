1b606f8618ca36f1ee8dba03a731406b8ccffa82
Will Bohman <williamemilbohman@gmail.com>
2026-09-12 15:01:00 -0700

    finished adding the rest of my codewars challenges

---
+++ 8kyu/Convert-Number-to-String.js
@@ -0,0 +1,5 @@
+//We need a function that can transform a number (integer) into a string.
+//My Solution
+function numberToString(num) {
+  return (num.toString());
+}
---
+++ 8kyu/Reversed-Strings.js
@@ -0,0 +1,10 @@
+//Complete the solution so that it reverses the string passed into it.
+//My Solution
+function solution(str){
+  return [...str].reverse().join('');
+}
+
+//Solution to study
+function solution(str){
+  return str.split('').reverse().join('');  
+}
